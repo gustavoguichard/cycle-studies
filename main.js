896d0d7f934bd2968a7390792168b22ec07ba79a
Gustavo Guichard <gustavoguichard@gmail.com>
2016-04-04 18:47:12 -0300

    Using cycle/dom

--- main.js
+++ main.js
@@ -1,54 +1,25 @@
 import Rx from 'rx'
 import Cycle from '@cycle/core'
-
-const h = (tagName, children) => ({ tagName, children })
-const h1 = children => h('H1', children)
-const span = children => h('SPAN', children)
+import {h, h1, span, makeDOMDriver} from '@cycle/dom'
 
 const main = sources => {
-  const mouseover$ = sources.DOM.selectEvents('span', 'mouseover')
+  const mouseover$ = sources.DOM
+    .select('span')
+    .events('mouseover')
   const sinks = {
     DOM: mouseover$
       .startWith(null)
       .flatMapLatest(() =>
         Rx.Observable.timer(0, 1000)
           .map(i =>
-            h1([
-              span([`seconds elapsed ${i}`])
+            h1({style: {background: 'red'}}, [
+              span(`seconds elapsed ${i}`)
             ]))
       ),
     Log: Rx.Observable.timer(0, 2000).map(i => 2*i)
   }
   return sinks
 }
-
-const makeDOMDriver = mountSelector => (
-  obj$ => {
-    const createElement = obj => {
-      const element = document.createElement(obj.tagName)
-      obj.children
-        .filter(c => typeof c === 'object')
-        .map(createElement)
-        .forEach(c => element.appendChild(c))
-      obj.children
-        .filter(c => typeof c === 'string')
-        .forEach(c => element.innerHTML = c)
-      return element
-    }
-    obj$.subscribe(obj => {
-      const container = document.querySelector(mountSelector)
-      container.innerHTML = ''
-      const element = createElement(obj)
-      container.appendChild(element)
-    })
-
-    return {
-      selectEvents: (tagName, eventType) =>
-        Rx.Observable.fromEvent(document, eventType)
-          .filter(ev => ev.target.tagName === tagName.toUpperCase())
-    }
-  }
-)
 
 const consoleLogDriver = msg$ =>
   msg$.subscribe(msg => console.log(msg))
