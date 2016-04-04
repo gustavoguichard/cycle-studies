4dd758e2636407d0be05f3d5f4e5124ab256eb61
Gustavo Guichard <gustavoguichard@gmail.com>
2016-04-04 20:51:13 -0300

    Increment and Decrement

--- src/main.js
+++ src/main.js
@@ -1,36 +1,71 @@
 import Rx from 'rx'
 import Cycle from '@cycle/core'
-import {h, div, hr, input, label, h1, span, makeDOMDriver} from '@cycle/dom'
+import {h, makeDOMDriver} from '@cycle/dom'
 
 const main = sources => {
-  const inputEv$ = sources.DOM.select('.field').events('input')
-  const name$ = inputEv$
-    .map(ev => ev.target.value)
-    .startWith('')
-  // const mouseover$ = sources.DOM.select('span').events('mouseover')
+  const decrementClick$ = sources.DOM
+    .select('.decrement').events('click')
+  const incrementClick$ = sources.DOM
+    .select('.increment').events('click')
+  const decrementAction$ = decrementClick$.map(ev => -1)
+  const incrementAction$ = incrementClick$.map(ev => +1)
+
+  const number$ = Rx.Observable.of(0)
+    .merge(decrementAction$)
+    .merge(incrementAction$)
+    .scan((prev, curr) => prev + curr)
+
   return {
-    DOM: name$
-    // mouseover$
-    // .startWith('')
-    // .flatMapLatest(() =>
-    //   Rx.Observable.timer(0, 1000)
-    //     .map(i =>
-    //       h1({style: {background: 'blue'}}, [
-    //         span(`seconds elapsed ${i}`)
-    //       ])
-    //     )
-    // ),
-    .map(name =>
-      div([
-        label('Name:'),
-        input('.field', {type: 'text'}),
-        hr(),
-        h1(`Hello ${name}!`)
+    DOM: number$.map(number =>
+      h('div', [
+        h('button.decrement', 'Decrement'),
+        h('button.increment', 'Increment'),
+        h('p',
+          h('label', String(number))
+        )
       ])
     )
-  };
+  }
 }
 
 Cycle.run(main, {
   DOM: makeDOMDriver('#app')
 })
+
+/* Clock
+const main = sources => {
+  const mouseover$ = sources.DOM.select('span').events('mouseover')
+  return {
+    mouseover$
+    .startWith('')
+    .flatMapLatest(() =>
+      Rx.Observable.timer(0, 1000)
+        .map(i =>
+          h('h1', {style: {background: 'blue'}}, [
+            h('span', `seconds elapsed ${i}`)
+          ])
+        )
+    ),
+  };
+}
+*/
+
+/* Hello World
+const main = sources => {
+  const inputEv$ = sources.DOM.select('.field').events('input')
+  const name$ = inputEv$
+    .map(ev => ev.target.value)
+    .startWith('')
+  return {
+    DOM: name$
+    .map(name =>
+      h('div', [
+        h('label', 'Name:'),
+        h('input.field', {type: 'text'}),
+        h('hr'),
+        h('h1',`Hello ${name}!`)
+      ])
+    )
+  };
+}
+*/
